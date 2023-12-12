c3a71f13a805b03521f3ccffe7a0fea5a76962ce
TRICKAL3 <153557959+TRICKAL3@users.noreply.github.com>
2023-12-12 17:47:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('signup-login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Your signup logic here...
+
+    // Assuming signup is successful, redirect to a new page
+    window.location.href = 'new-page.html';
+});
